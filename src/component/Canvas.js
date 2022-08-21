fdd17a0a1e8ab1678e0ba7de384bf641fcf10bf7
yujinkim11 <wowyj26@naver.com>
2022-08-21 18:47:46 +0900

    fillRect

--- src/component/Canvas.js
+++ src/component/Canvas.js
@@ -2,12 +2,22 @@ import { useEffect, useRef, useState } from "react";
 
 export const Canvas = () => {
   const canvasRef = useRef(null);
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
+  const [getCtx, setGetCtx] = useState(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    canvas.width = 800;
+    canvas.height = 800;
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#000000";
+    setGetCtx(ctx);
+    ctx.fillRect(50, 50, 100, 100);
+  }, []);
 
   return (
     <div className="canvasWrap">
-      <canvas ref={canvasRef} width={800} height={800}></canvas>
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
 };
